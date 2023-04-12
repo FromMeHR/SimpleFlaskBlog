0522b1354b6fda741ce8d1f8df6788820d648efe
Maxim <126081409+Maxim-Levchenko@users.noreply.github.com>
2023-04-12 22:54:40 +0300

    Update schema.sql

--- blog/schema.sql
+++ blog/schema.sql
@@ -11,5 +11,6 @@ CREATE TABLE posts (
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL
+    last_name TEXT NOT NULL,
+    password TEXT NOT NULL
 );
